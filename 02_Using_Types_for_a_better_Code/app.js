5dc5a36a22909ed2e922ca2710855570bc4ede8c
solareenlo <solareenlo@gmail.com>
2018-12-22 04:51:13 +0900

    変数の型の明示のやり方. 型を示さないとnum, strどちらでも使える.

--- 02_Using_Types_for_a_better_Code/app.js
+++ 02_Using_Types_for_a_better_Code/app.js
@@ -1,11 +1,18 @@
 "use strict";
 // string
 let myName = 'sola';
-myName = 20;
+// myName = 20; // $tsc するとエラーになる.
 // number
 let myAge = 27;
-myAge = 'sola';
+// myAge = 'sola'; // $tsc するとエラーになる.
 // boolean
 let hasFoods = false;
-hasFoods = 2;
+// hasFoods = 2; // $tsc するとエラーになる.
+// assign types
+let myRealAge;
+myRealAge = 27;
+myRealAge = '28'; // これはエラーにならない.
+let myRealAge2;
+myRealAge2 = 27;
+myRealAge2 = '27'; // これはエラーになる. 型を明示しているため.
 //# sourceMappingURL=app.js.map
--- 02_Using_Types_for_a_better_Code/app.ts
+++ 02_Using_Types_for_a_better_Code/app.ts
@@ -1,11 +1,20 @@
 // string
-let myName = 'sola';
-myName = 20;
+let myName: string = 'sola';
+// myName = 20; // $tsc するとエラーになる.
 
 // number
-let myAge = 27;
-myAge = 'sola';
+let myAge: number = 27;
+// myAge = 'sola'; // $tsc するとエラーになる.
 
 // boolean
-let hasFoods = false;
-hasFoods = 2;
+let hasFoods: boolean = false;
+// hasFoods = 2; // $tsc するとエラーになる.
+
+// assign types
+let myRealAge;
+myRealAge = 27;
+myRealAge = '28'; // これはエラーにならない.
+
+let myRealAge2: number;
+myRealAge2 = 27;
+myRealAge2 = '27'; // これはエラーになる. 型を明示しているため.
