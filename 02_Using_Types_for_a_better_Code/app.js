932ad157befa04aa7f2fcacf85c9c93214687ad7
solareenlo <solareenlo@gmail.com>
2018-12-22 05:02:15 +0900

    配列は中身の型を指定する.

--- 02_Using_Types_for_a_better_Code/app.js
+++ 02_Using_Types_for_a_better_Code/app.js
@@ -15,4 +15,10 @@ myRealAge = '28'; // これはエラーにならない.
 let myRealAge2;
 myRealAge2 = 27;
 myRealAge2 = '27'; // これはエラーになる. 型を明示しているため.
+// array
+let hobbies = ["Cooking", "Sports"];
+hobbies = [100]; // これはエラーになる. 配列の中身がString型では無いので.
+let hobbies2 = ["Cooking", "Sports"];
+hobbies2 = [100]; // これはエラーにならない. 配列の中身の型がanyになっているので.
+hobbies2 = 100; // これはエラーになる. 配列では無いので.
 //# sourceMappingURL=app.js.map
--- 02_Using_Types_for_a_better_Code/app.ts
+++ 02_Using_Types_for_a_better_Code/app.ts
@@ -18,3 +18,11 @@ myRealAge = '28'; // これはエラーにならない.
 let myRealAge2: number;
 myRealAge2 = 27;
 myRealAge2 = '27'; // これはエラーになる. 型を明示しているため.
+
+// array
+let hobbies = ["Cooking", "Sports"];
+hobbies = [100]; // これはエラーになる. 配列の中身がString型では無いので.
+
+let hobbies2: any[] = ["Cooking", "Sports"];
+hobbies2 = [100]; // これはエラーにならない. 配列の中身の型がanyになっているので.
+hobbies2 = 100; // これはエラーになる. 配列では無いので.
