00a7cfc61f27f7f83e68b6e745eb68e54f6f85a4
solareenlo <solareenlo@gmail.com>
2018-12-27 07:49:16 +0900

    privateに設定したものは外部からはアクセスできない

--- 04_Using_Classes_to_create_Objects/app.js
+++ 04_Using_Classes_to_create_Objects/app.js
@@ -2,8 +2,20 @@
 class Person {
     constructor(name, username) {
         this.username = username;
+        this.type = 'test';
+        this.age = 27;
         this.name = name;
+    }
+    printAge() {
+        console.log(this.age);
+        this.setType('Old Guy');
+    }
+    setType(type) {
+        this.type = type;
+        console.log(this.type);
     }
 }
 const person = new Person('sola', 'Sola');
-console.log(person.name, person.username);
+console.log(person.name, person.username); // sola Sola と表示
+person.printAge();
+// person.setType('Cool guy'); // setTypeメソッドがprivateのため使えない
--- 04_Using_Classes_to_create_Objects/app.ts
+++ 04_Using_Classes_to_create_Objects/app.ts
@@ -1,12 +1,24 @@
 class Person {
   name: string;
-  private type: string;
-  protected age: number;
+  private type: string = 'test';
+  protected age: number = 27;
 
   constructor(name: string, public username: string) {
     this.name = name;
   }
+
+  printAge() {
+    console.log(this.age);
+    this.setType('Old Guy');
+  }
+
+  private setType(type: string) {
+    this.type = type;
+    console.log(this.type);
+  }
 }
 
 const person = new Person('sola', 'Sola');
-console.log(person.name, person.username);
+console.log(person.name, person.username); // sola Sola と表示
+person.printAge();
+// person.setType('Cool guy'); // setTypeメソッドがprivateのため使えない
