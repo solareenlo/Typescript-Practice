96e5ce56e06a977dbb6bc0eed549016c0c8d4d60
solareenlo <solareenlo@gmail.com>
2018-12-27 02:12:28 +0900

    rest演算子を用いて塊にする例

--- 03_TypeScript_and_ES6/app.js
+++ 03_TypeScript_and_ES6/app.js
@@ -8,9 +8,13 @@ const countdown = (start = 10) => {
     }
     console.log("Done", start);
 };
-countdown();
+countdown(); // 10 Done 0 と縦に表示
 // Rest & Spread
 console.log("REST & SPREAD");
 const numbers = [1, 2, 10, -5];
-console.log(Math.max(33, 30, 99, -14));
-console.log(Math.max(...numbers));
+console.log(Math.max(33, 30, 99, -14)); // 99 と表示
+console.log(Math.max(...numbers)); // 10 と表示 spread演算子を用いてバラバラにしてる
+function makeArray(name, ...args) {
+    return args;
+}
+console.log(makeArray("sola", 1, 2, 3)); // [1, 2, 3] と表示
--- 03_TypeScript_and_ES6/app.ts
+++ 03_TypeScript_and_ES6/app.ts
@@ -13,4 +13,9 @@ countdown(); // 10 Done 0 と縦に表示
 console.log("REST & SPREAD");
 const numbers: number[] = [1, 2, 10, -5];
 console.log(Math.max(33, 30, 99, -14)); // 99 と表示
-console.log(Math.max(...numbers)); // 10 と表示
+console.log(Math.max(...numbers)); // 10 と表示 spread演算子を用いてバラバラにしてる
+
+function makeArray(name: string, ...args: number[]): number[] { // rest演算子を用いて塊にしてる
+  return args;
+}
+console.log(makeArray("sola", 1, 2, 3)); // [1, 2, 3] と表示
