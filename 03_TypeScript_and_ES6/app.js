83ff48218e7c9be10b13fc72094a9a71b4ac182c
solareenlo <solareenlo@gmail.com>
2018-12-27 02:19:03 +0900

    関数の引数を, rest演算子を用いて, tuple型に収めることもできる

--- 03_TypeScript_and_ES6/app.js
+++ 03_TypeScript_and_ES6/app.js
@@ -18,3 +18,14 @@ function makeArray(name, ...args) {
     return args;
 }
 console.log(makeArray("sola", 1, 2, 3)); // [1, 2, 3] と表示
+// 関数の引数として, tuple型でrest演算子を使用する例
+function printInfo(name, age) {
+    console.log(`My name is ${name} and I am ${age} years old.`);
+    // My name is sola and I am 25 years old. と表示
+}
+printInfo('sola', 25);
+function printInfo2(...info) {
+    console.log(`My name is ${info[0]} and I am ${info[1]} years old.`);
+    // My name is sola and I am 25 years old. と表示
+}
+printInfo2('sola', 25);
--- 03_TypeScript_and_ES6/app.ts
+++ 03_TypeScript_and_ES6/app.ts
@@ -19,3 +19,16 @@ function makeArray(name: string, ...args: number[]): number[] { // rest演算子
   return args;
 }
 console.log(makeArray("sola", 1, 2, 3)); // [1, 2, 3] と表示
+
+// 関数の引数として, tuple型でrest演算子を使用する例
+function printInfo(name: string, age: number): void {
+  console.log(`My name is ${name} and I am ${age} years old.`);
+  // My name is sola and I am 25 years old. と表示
+}
+printInfo('sola', 25);
+
+function printInfo2(...info: [string, number]): void {
+  console.log(`My name is ${info[0]} and I am ${info[1]} years old.`);
+  // My name is sola and I am 25 years old. と表示
+}
+printInfo2('sola', 25);
