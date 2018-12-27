3dc44ff6e62abb9e8cdb0c30df1dc7509a07ad0e
solareenlo <solareenlo@gmail.com>
2018-12-28 07:40:30 +0900

    interfaceでanyを使ってみた

--- 06_Doing_contract_Work_with_Interfaces/app.js
+++ 06_Doing_contract_Work_with_Interfaces/app.js
@@ -7,8 +7,10 @@ function changeName(person) {
 }
 var person = {
     name: 'sola',
-    age: 27
+    age: 27,
+    hobbies: ['Cooking', 'Sports']
 };
 greet(person); // Hello, sola. と表示
 changeName(person);
 greet(person); // Hello, Taro. と表示
+greet({ name: 'sola', age: 27 });
--- 06_Doing_contract_Work_with_Interfaces/app.ts
+++ 06_Doing_contract_Work_with_Interfaces/app.ts
@@ -1,5 +1,7 @@
 interface NamedPerson {
   name: string;
+  age: number;
+  [propName: string]: any;
 }
 
 function greet(person: NamedPerson) {
@@ -10,11 +12,13 @@ function changeName(person: NamedPerson) {
   person.name = 'Taro';
 }
 
-const person: {name: string, age: number} = {
+const person: {name: string, age: number, hobbies: string[]} = {
   name: 'sola',
-  age: 27
+  age: 27,
+  hobbies: ['Cooking', 'Sports']
 }
 
 greet(person); // Hello, sola. と表示
 changeName(person);
 greet(person); // Hello, Taro. と表示
+greet({name: 'sola', age: 27}); // Hello, sola. と表示
