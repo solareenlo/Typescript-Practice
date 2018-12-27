2418527f34813f4dde0d03a351873ebaa156066a
solareenlo <solareenlo@gmail.com>
2018-12-28 05:25:17 +0900

    モジュールの使い方 exportとimportを使います

---
+++ 05_Namespaces_and_Modules2/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var circle_1 = require("./math/circle");
+console.log(circle_1.PI); // 3.14 と表示
+console.log(circle_1.calCirumcumference(10)); // 31.4 と表示
---
+++ 05_Namespaces_and_Modules2/app.ts
@@ -0,0 +1,4 @@
+import { PI, calCirumcumference } from "./math/circle"
+
+console.log(PI); // 3.14 と表示
+console.log(calCirumcumference(10)); // 31.4 と表示
