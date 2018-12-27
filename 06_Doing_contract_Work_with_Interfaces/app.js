fdb45170d00c2bcc2e0146184d151364bb2a071c
solareenlo <solareenlo@gmail.com>
2018-12-28 07:46:19 +0900

    interfaceでfunctionを使用

--- 06_Doing_contract_Work_with_Interfaces/app.js
+++ 06_Doing_contract_Work_with_Interfaces/app.js
@@ -8,9 +8,13 @@ function changeName(person) {
 var person = {
     name: 'sola',
     age: 27,
-    hobbies: ['Cooking', 'Sports']
+    hobbies: ['Cooking', 'Sports'],
+    greet: function (lastName) {
+        console.log("Hi, I am " + this.name + " " + lastName + ".");
+    }
 };
 greet(person); // Hello, sola. と表示
 changeName(person);
 greet(person); // Hello, Taro. と表示
-greet({ name: 'sola', age: 27 });
+// greet({name: 'sola', age: 27}); // Hello, sola. と表示
+person.greet('Yamada'); // Hi, I am Taro Yamada. と表示
--- 06_Doing_contract_Work_with_Interfaces/app.ts
+++ 06_Doing_contract_Work_with_Interfaces/app.ts
@@ -2,6 +2,7 @@ interface NamedPerson {
   name: string;
   age: number;
   [propName: string]: any;
+  greet(lastName: string): void;
 }
 
 function greet(person: NamedPerson) {
@@ -12,13 +13,17 @@ function changeName(person: NamedPerson) {
   person.name = 'Taro';
 }
 
-const person: {name: string, age: number, hobbies: string[]} = {
+const person: NamedPerson = {
   name: 'sola',
   age: 27,
-  hobbies: ['Cooking', 'Sports']
+  hobbies: ['Cooking', 'Sports'],
+  greet(lastName: string) {
+    console.log(`Hi, I am ${this.name} ${lastName}.`);
+  }
 }
 
 greet(person); // Hello, sola. と表示
 changeName(person);
 greet(person); // Hello, Taro. と表示
-greet({name: 'sola', age: 27}); // Hello, sola. と表示
+// greet({name: 'sola', age: 27}); // Hello, sola. と表示
+person.greet('Yamada'); // Hi, I am Taro Yamada. と表示
