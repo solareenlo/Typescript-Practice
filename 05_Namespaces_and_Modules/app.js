406830528b06ffdafe2ec6a3bcdeaeaf83600aac
solareenlo <solareenlo@gmail.com>
2018-12-28 04:44:13 +0900

    分割された名前空間をimportするには///を使えば良い

--- 05_Namespaces_and_Modules/app.js
+++ 05_Namespaces_and_Modules/app.js
@@ -17,6 +17,8 @@ var MyMath;
 // $ tsc --outFile app.js circle.ts rectangle.ts app.ts --types node
 // .tsファイルの入力順番を間違えないこと.
 // .tsファイルを入力した順番にコンパルされたjsコードがapp.jsに追加されます.
+/// <reference path="circle.ts" />
+/// <reference path="rectangle.ts" />
 var PI = 2.23847928374;
 console.log(MyMath.calCircumference(10)); // 31.400000000000002 と表示
 console.log(MyMath.calRectangle(10, 20)); // 200 と表示
--- 05_Namespaces_and_Modules/app.ts
+++ 05_Namespaces_and_Modules/app.ts
@@ -2,6 +2,8 @@
 // $ tsc --outFile app.js circle.ts rectangle.ts app.ts --types node
 // .tsファイルの入力順番を間違えないこと.
 // .tsファイルを入力した順番にコンパルされたjsコードがapp.jsに追加されます.
+/// <reference path="circle.ts" />
+/// <reference path="rectangle.ts" />
 const PI: number = 2.23847928374;
 
 console.log(MyMath.calCircumference(10)); // 31.400000000000002 と表示
