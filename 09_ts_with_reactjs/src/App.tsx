f58bc1df5873baca94806b5fe989ec42bce200b7
solareenlo <solareenlo@gmail.com>
2018-12-29 10:43:58 +0900

    1つ増やす, 1つ減らすボタンを実装

--- 09_ts_with_reactjs/src/App.tsx
+++ 09_ts_with_reactjs/src/App.tsx
@@ -2,10 +2,34 @@ import * as React from 'react';
 
 import CounterOutput from './CounterOutput';
 
-class App extends React.Component {
+interface IAppState {
+  counterValue: number;
+}
+
+class App extends React.Component<{}, IAppState> {
+  public state = { counterValue: 0 };
+
   public render() {
-    return <CounterOutput counter={1}/>;
-  }
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <CounterOutput counter={this.state.counterValue}/>
+        <button onClick={this.incHandler}>Increment</button>
+        <button onClick={this.decHandler}>Decrement</button>
+      </div>
+    );
+  };
+
+  private incHandler = () => {
+    this.setState(prevState => {
+      return { counterValue: prevState.counterValue + 1 };
+    });
+  };
+
+  private decHandler = () => {
+    this.setState(prevState => {
+      return { counterValue: prevState.counterValue - 1 };
+    });
+  };
 }
 
 export default App;
