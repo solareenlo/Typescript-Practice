0554b6d342f17a6f9a3059608f187ec643665b0c
solareenlo <solareenlo@gmail.com>
2018-12-29 10:33:13 +0900

    とりあえず1を表示

---
+++ 09_ts_with_reactjs/README.md
@@ -0,0 +1,4 @@
+# Using TypeScript togeter with ReactJS
+
+## yarn buil や yarn start でエラーが出たときは,
+tsconfig.jsonの"compilerOptions"のところに`"types": ["node", "jest"]`を付け加えてみる.
---
+++ 09_ts_with_reactjs/src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import CounterOutput from './CounterOutput';
+
+class App extends React.Component {
+  public render() {
+    return <CounterOutput counter={1}/>;
+  }
+}
+
+export default App;
